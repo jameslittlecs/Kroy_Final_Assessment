cbcb49c1c5b5735ae09360ee0e6fe2fcb8b99bff
jameslittlecs <jl2831@york.ac.uk>
2020-04-27 20:49:59 +0100

    Pause Button and Shader File removed

--- Kroy/core/src/com/mozarellabytes/kroy/Utilities/GUI.java
+++ Kroy/core/src/com/mozarellabytes/kroy/Utilities/GUI.java
@@ -326,13 +326,11 @@ public class GUI {
      * and unpauses the game */
     public void clickedPauseButton() {
 
-        if (gameScreen.getState().equals(GameScreen.PlayState.PLAY)) {
-            currentPauseTexture = pauseButtonClicked;
+        if (gameScreen.getState().equals(GameScreen.PlayState.PLAY)) {    
             if (SoundFX.music_enabled) {
                 SoundFX.sfx_pause.play();
             }
         } else {
-            currentPauseTexture = pauseButtonIdle;
             if (SoundFX.music_enabled) {
                 SoundFX.sfx_unpause.play();
             }
